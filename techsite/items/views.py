a2cf6e49d0734da1c75b1bbafc1caf734aeb18f8
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-17 18:31:25 +0000

    Removed old site

--- techsite/items/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Category
-# Create your views here.
-def display_items(request):
-    categories = Category.objects.all()
-    print(categories)
-    return render(request, "items/item_list.html", {
-        "categories": categories,
-    })

40ddefde0a6171b45f702e7d3b1bc6eae0abac2d
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-17 20:39:24 +0000

    Remove print statements

--- infopages/views.py
+++ infopages/views.py
@@ -3,11 +3,8 @@ from .models import InfoPage
 # Create your views here.
 def view_page(request, slug):
     # Look up the slug.
-    print(slug)
     page = InfoPage.objects.get(slug=slug)
     page_data = {
         "page": page
     }
     return render(request, 'infopages/view_page.html', page_data)
-    # If found, get page and display.
-    # Otherwise, 404.
